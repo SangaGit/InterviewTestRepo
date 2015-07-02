c470af1caa05afd6afbd3b7aea21ba690b635d85
Sangeeth Adhikari <dilushasanga@gmail.com>
2015-07-02 09:25:52 +0530

    For the Segment1

---
+++ ConsoleApplication(Segment1)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_Segment1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " - Support Training");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " - Support");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " - Training");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
